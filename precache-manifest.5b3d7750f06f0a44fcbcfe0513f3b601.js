1d58e3f013eb7378c95c4d13bcbfef937f7ea04b
Daniel Don <dandons25@gmail.com>
2019-04-13 15:38:59 +0100

    Updates

---
+++ precache-manifest.5b3d7750f06f0a44fcbcfe0513f3b601.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ec4b692eb032bcf721b4d34b7fbbb9f",
+    "url": "/weatherguage/static/media/weather-guage.9ec4b692.png"
+  },
+  {
+    "revision": "7e594dbe91c37eea8d42",
+    "url": "/weatherguage/static/js/runtime~main.7e594dbe.js"
+  },
+  {
+    "revision": "407ebadcfd461e3f13d1",
+    "url": "/weatherguage/static/js/main.407ebadc.chunk.js"
+  },
+  {
+    "revision": "d5bb48482e4a8128da8c",
+    "url": "/weatherguage/static/js/1.d5bb4848.chunk.js"
+  },
+  {
+    "revision": "407ebadcfd461e3f13d1",
+    "url": "/weatherguage/static/css/main.ef2db74a.chunk.css"
+  },
+  {
+    "revision": "02f039a1c095e8d29f550f63d34f041f",
+    "url": "/weatherguage/index.html"
+  }
+];
